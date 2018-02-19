d407b1c7e4d3dfcb0004ccaf492e53b6243e4afa
MarcoFavorito <marco.favorito@gmail.com>
2018-02-19 01:01:06 +0100

    added atom and keybase installations

---
+++ atom/install.sh
@@ -0,0 +1,6 @@
+#http://flight-manual.atom.io/getting-started/sections/installing-atom/
+curl -L https://packagecloud.io/AtomEditor/atom/gpgkey | sudo apt-key add -
+sudo sh -c 'echo "deb [arch=amd64] https://packagecloud.io/AtomEditor/atom/any/ any main" > /etc/apt/sources.list.d/atom.list'
+sudo apt-get update
+# Install Atom
+sudo apt-get install atom
