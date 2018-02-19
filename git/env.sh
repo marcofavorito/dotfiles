54fe0ecbbbe626fd4f11b6ef2453b90bce73b682
MarcoFavorito <marco.favorito@gmail.com>
2018-02-19 01:10:26 +0100

    aliases for git

---
+++ git/env.sh
@@ -0,0 +1,7 @@
+# shortcuts for git
+alias ga="git add"
+alias gb="git branch"
+alias gd="git diff --patience --ignore-space-change"
+alias gh="git log --pretty=format:\"%Cgreen%h%Creset %Cblue%ad%Creset %s%C(yellow)%d%Creset %Cblue[%an]%Creset\" --graph --date=short"
+alias gc="git checkout"
+alias gs="git status"
