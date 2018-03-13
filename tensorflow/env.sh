0fbe04d8b817ab5d3c43c0bb113e8b1c1287e33f
Marco Favorito <marco.favorito@gmail.com>
2018-03-13 21:26:39 +0100

    envars for TF

---
+++ tensorflow/env.sh
@@ -0,0 +1,9 @@
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda/extras/CUPTI/lib64
+
+#export PATH=/usr/local/cuda-9.1/bin${PATH:+:${PATH}}
+#export LD_LIBRARY_PATH=/usr/local/cuda-9.1/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
+
+export LD_LIBRARY_PATH=/usr/local/cuda-9.0/lib64\${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
+export PATH=/usr/local/cuda-9.0/bin${PATH:+:${PATH}}
+
+
