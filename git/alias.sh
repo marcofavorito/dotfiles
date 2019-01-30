eb939fcaf8d64b16013684dfa505d03a0cddd823
MarcoFavorito <marco.favorito@gmail.com>
2019-01-30 17:24:44 +0000

    add git alias.

--- git/alias.sh
+++ git/alias.sh
@@ -9,8 +9,10 @@ alias gs="git status"
 alias gits="git commit -S -m $1"
 alias gpom="git push origin master"
 alias gpod="git push origin develop"
+alias gpll="git pull origin master"
 
 git config --global alias.pullall '!f(){ git pull "$@" && git submodule sync --recursive && git submodule update --init --recursive; }; f'
 git config --global alias.syncUpdate '!f(){ git submodule sync --recursive && git submodule update --init --recursive; }; f'
 git config --global alias.logg '!f(){ git log --graph --stat --decorate=full --color --word-diff=color -M -C --find-copies-harder -l100 --histogram "$@"; }; f'
 git config --global alias.oldest-ancestor $'!zsh -c \'diff --old-line-format= --new-line-format= <(git rev-list --first-parent \"${1:-master}\") <(git rev-list --first-parent \"${2:-HEAD}\") | head -1\' -'
+
