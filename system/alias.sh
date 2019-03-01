52bf2448028d40053dc11c21bb2cce23e544b733
MarcoFavorito <marco.favorito@gmail.com>
2019-03-01 22:50:08 +0000

    add enumerate alias.

--- system/alias.sh
+++ system/alias.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+# mist utils
 alias cclip='xclip -selection clipboard'
 alias copyLastCmd="fc -ln -1 | awk '{\$1=\$1}1' | cclip "
 
@@ -7,7 +8,9 @@ function getPod() {
     kubectl get pods | grep $1 | cut -d' ' -f1 
 }
 
+alias enumerate="awk '{print NR-1 \" \" \$0}'"
 
 # REPLACE rm wit rmtrash
 alias rm='rmtrash'
 alias rmdir='rmdirtrash'
+
