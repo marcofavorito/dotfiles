8863ab255a78a7caccc7778b54ca64e120121490
MarcoFavorito <marco.favorito@gmail.com>
2019-03-02 20:14:21 +0000

    ad awk alias for computing average and variance from a list of numbers.

--- system/alias.sh
+++ system/alias.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# mist utils
+# misc utils
 alias cclip='xclip -selection clipboard'
 alias copyLastCmd="fc -ln -1 | awk '{\$1=\$1}1' | cclip "
 
@@ -8,7 +8,10 @@ function getPod() {
     kubectl get pods | grep $1 | cut -d' ' -f1 
 }
 
+# useful awks!
 alias enumerate="awk '{print NR-1 \" \" \$0}'"
+alias avg="awk '{ print \$1 }' | awk '{s+=\$1}END{print s/NR}'"
+alias var="awk '{total+=\$1 ; sq+=\$1*\$1 ; }END{ print sq/NR-(total/NR)**2 ; }'"
 
 # REPLACE rm wit rmtrash
 alias rm='rmtrash'
