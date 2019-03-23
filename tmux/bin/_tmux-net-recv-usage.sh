253a79249b19bc495e1e29d2af244b03697d773e
MarcoFavorito <marco.favorito@gmail.com>
2019-03-23 01:20:00 +0100

    fix tmux status bar script for network

--- tmux/bin/_tmux-net-recv-usage.sh
+++ tmux/bin/_tmux-net-recv-usage.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-awk '{if(l1){printf "%010.3f kB/s \n", ($2-l1)/1024} else{l1=$2; l2=$10;}}' <(grep wlp59s0 /proc/net/dev) <(sleep 1; grep wlp59s0 /proc/net/dev)
+:wk '{if(l1){printf "%010.3f kB/s \n", ($2-l1)/1024} else{l1=$2; l2=$10;}}' <(grep wlp /proc/net/dev) <(sleep 1; grep wlp /proc/net/dev)
 
