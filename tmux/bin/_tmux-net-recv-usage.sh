b1e251c1dc2a1becfb42096bbec091ec3dcbb419
MarcoFavorito <marco.favorito@gmail.com>
2019-04-07 10:43:02 +0200

    small fix.

--- tmux/bin/_tmux-net-recv-usage.sh
+++ tmux/bin/_tmux-net-recv-usage.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-:wk '{if(l1){printf "%010.3f kB/s \n", ($2-l1)/1024} else{l1=$2; l2=$10;}}' <(grep wlp /proc/net/dev) <(sleep 1; grep wlp /proc/net/dev)
+awk '{if(l1){printf "%010.3f kB/s \n", ($2-l1)/1024} else{l1=$2; l2=$10;}}' <(grep wlp /proc/net/dev) <(sleep 1; grep wlp /proc/net/dev)
 
