dd9726ec94a84e5387b168bc7bee2d77e4b62d04
MarcoFavorito <marco.favorito@gmail.com>
2019-05-25 16:54:34 +0200

    fix in .bashrc for tmux

--- pip/install.sh
+++ pip/install.sh
@@ -1,5 +1,6 @@
 
 sudo apt-get install -y python3-pip 
+sudo python3 -m pip install --upgrade pip
 sudo python3 -m pip install --no-cache-dir -U pipenv 
 sudo python3 -m pip install --no-cache-dir -U virtualenv
 sudo python3 -m pip install --no-cache-dir -U virtualenvwrapper
