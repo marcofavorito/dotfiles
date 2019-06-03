1560a2874720f80ee566782d3f810a48efb2cbb5
MarcoFavorito <marco.favorito@gmail.com>
2019-06-03 16:43:39 +0200

    add cuda folder.

---
+++ cuda/README.md
@@ -0,0 +1,5 @@
+## Useful links
+
+- [CUDA Nvidia download page](https://docs.nvidia.com/cuda/cuda-installation-guide-linux/index.html)
+- [CUDA installation guide](https://docs.nvidia.com/cuda/cuda-installation-guide-linux/index.html)
+
---
+++ cuda/env.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+
+export PATH=/usr/local/cuda-10.1/bin:/usr/local/cuda-10.1/NsightCompute-2019.1${PATH:+:${PATH}}
+export LD_LIBRARY_PATH=/usr/local/cuda-10.1/lib64\
+                         ${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
+
