2ccbc0fc6fa62c23f38819faacc4063442974f87
MarcoFavorito <marco.favorito@gmail.com>
2019-06-14 20:11:01 +0100

    fix cuda env.sh

--- cuda/env.sh
+++ cuda/env.sh
@@ -2,6 +2,5 @@
 
 
 export PATH=/usr/local/cuda-10.1/bin:/usr/local/cuda-10.1/NsightCompute-2019.1${PATH:+:${PATH}}
-export LD_LIBRARY_PATH=/usr/local/cuda-10.1/lib64\
-                         ${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
+export LD_LIBRARY_PATH=/usr/local/cuda-10.1/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
 
