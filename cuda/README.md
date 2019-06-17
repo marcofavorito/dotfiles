4a5b7f5f0525af4c5861710abf2f433718c766d8
MarcoFavorito <marco.favorito@gmail.com>
2019-06-17 22:35:02 +0100

    fix cuda env and add link for cuDNN.

--- cuda/README.md
+++ cuda/README.md
@@ -3,3 +3,4 @@
 - [CUDA Nvidia download page](https://docs.nvidia.com/cuda/cuda-installation-guide-linux/index.html)
 - [CUDA installation guide](https://docs.nvidia.com/cuda/cuda-installation-guide-linux/index.html)
 
+- [cuDNN installation guide](https://docs.nvidia.com/deeplearning/sdk/cudnn-install/index.html)
--- cuda/env.sh
+++ cuda/env.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
+export CUDA_VERSION=10.0
 
-export PATH=/usr/local/cuda-10.1/bin:/usr/local/cuda-10.1/NsightCompute-2019.1${PATH:+:${PATH}}
-export LD_LIBRARY_PATH=/usr/local/cuda-10.1/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
+export PATH=/usr/local/cuda-${CUDA_VERSION}/bin:/usr/local/cuda-${CUDA_VERSION}/NsightCompute-2019.1${PATH:+:${PATH}}
+export LD_LIBRARY_PATH=/usr/local/cuda-${CUDA_VERSION}/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
 
