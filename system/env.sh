2dc6ff145eec4ba04a8ef170e22c916dc51f7dd9
MarcoFavorito <marco.favorito@gmail.com>
2019-08-05 22:17:24 +0100

    add useful env vars.

--- system/env.sh
+++ system/env.sh
@@ -2,6 +2,7 @@ export OS="linux"
 export ARCH="amd64"
 export ARCH_ALT="x86_64"
 export WORKFOLDER="$HOME/workfolder"
+export DOTFILES="$HOME/.dotfiles"
 
 export DISTRO_VERSION=$(lsb_release -sc)
 
