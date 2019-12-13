db6a42a003aa53631ec09923ea69c90221000240
MarcoFavorito <marco.favorito@gmail.com>
2019-12-13 13:35:33 +0100

    update papis data.

--- papis/alias.sh
+++ papis/alias.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
-alias fromdoi="papis add --from doi"
+alias fromdoi="papis add --no-confirm --no-open --no-edit --from doi"
+alias fromarxiv="papis add --no-confirm --no-open --no-edit --from arxiv"
 
 function fromurl() {
-    papis add --set url "$1" --set title "$2" --set tags "$3"
+    papis add --no-confirm --no-open --no-edit --set url "$1" --set title "$2" --set tags "$3" --set author "$4" --set year "$5"
 }
 
