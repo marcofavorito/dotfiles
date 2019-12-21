8656e7a239f871c7c8fe7a2a99acedf47ba06db1
MarcoFavorito <marco.favorito@gmail.com>
2019-12-21 12:13:11 +0100

    update preliminaries to install docker.

--- system/preliminaries.sh
+++ system/preliminaries.sh
@@ -2,9 +2,35 @@
 
 sudo apt update && sudo apt upgrade -y
 sudo apt install git -y
-sudo apt install svn -y
+sudo apt install subversion -y
 sudo apt install vim -y
 sudo apt install golang -y
 sudo apt install curl -y
 
+# install Docker on Ubuntu:
+# https://docs.docker.com/install/linux/docker-ce/ubuntu/
+
+sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+
+# TODO for Ubuntu 19.10 there's no release. so use 19.04 release
+# sudo add-apt-repository \
+#    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+#    $(lsb_release -cs) \
+#    stable"
+
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   disco \
+   stable"
+
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
 
