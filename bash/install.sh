18fa421e59a08339b76dbd432c96c44ff582bad1
MarcoFavorito <marco.favorito@gmail.com>
2019-12-21 13:08:11 +0100

    remove 'zsh-completions' as submodule.

--- bash/install.sh
+++ bash/install.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
 
-# add completion to custom plugin zsh-completions in ohmyzsh 
-svn export https://github.com/littleq0903/gcloud-zsh-completion.git/trunk/src $ZSH_CUSTOM/plugins/zsh-completions/src --force
-
 if [ ! -f $HOME/.oh-my-zsh ]; then
     git clone https://github.com/ohmyzsh/ohmyzsh/ $HOME/.oh-my-zsh
 else
     cd $HOME/.oh-my-zsh && git pull origin master && cd -
 fi
+
