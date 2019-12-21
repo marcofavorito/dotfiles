f54cd42bdca677f96f597763ae5ba758baafe50c
MarcoFavorito <marco.favorito@gmail.com>
2019-12-21 12:37:43 +0100

    fix bash install when .oh-my-zsh already exists.

--- bash/install.sh
+++ bash/install.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
 # add completion to custom plugin zsh-completions in ohmyzsh 
-svn export https://github.com/littleq0903/gcloud-zsh-completion.git/trunk/src $ZSH/custom/plugins/zsh-completions/src --force
+svn export https://github.com/littleq0903/gcloud-zsh-completion.git/trunk/src $ZSH_CUSTOM/plugins/zsh-completions/src --force
 
-git clone https://github.com/ohmyzsh/ohmyzsh/ $HOME/.oh-my-zsh
-
+if [ ! -f $HOME/.oh-my-zsh ]; then
+    git clone https://github.com/ohmyzsh/ohmyzsh/ $HOME/.oh-my-zsh
+else
+    cd $HOME/.oh-my-zsh && git pull origin master && cd -
+fi
