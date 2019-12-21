35577da23ba5c6c9ceab23002f4277af10a26619
MarcoFavorito <marco.favorito@gmail.com>
2019-12-21 01:49:29 +0100

    remove oh-my-zsh submodule, track only custom.

--- bash/install.sh
+++ bash/install.sh
@@ -2,3 +2,6 @@
 
 # add completion to custom plugin zsh-completions in ohmyzsh 
 svn export https://github.com/littleq0903/gcloud-zsh-completion.git/trunk/src $ZSH/custom/plugins/zsh-completions/src --force
+
+git clone https://github.com/ohmyzsh/ohmyzsh/ $HOME/.oh-my-zsh
+
