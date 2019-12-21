800581fce163d82180d7905e55900d909e82aec0
MarcoFavorito <marco.favorito@gmail.com>
2019-12-21 13:13:48 +0100

    add zsh-completions as subtree, and add gcloud completions.

--- bash/install.sh
+++ bash/install.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
-if [ ! -f $HOME/.oh-my-zsh ]; then
+if [ ! -d $HOME/.oh-my-zsh ]; then
     git clone https://github.com/ohmyzsh/ohmyzsh/ $HOME/.oh-my-zsh
 else
     cd $HOME/.oh-my-zsh && git pull origin master && cd -
 fi
+
+svn export https://github.com/littleq0903/gcloud-zsh-completion.git/trunk/src "$ZSH_CUSTOM/plugins/zsh-completions/src" --force
 
