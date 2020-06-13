eaf427f865fe393a3b25805966898b755d4a47f2
MarcoFavorito <marco.favorito@gmail.com>
2020-06-13 17:46:25 +0200

    add pipx, update .latexmk

--- system/install.sh
+++ system/install.sh
@@ -18,6 +18,7 @@ sudo apt-get install -y virtualbox &&\
 # Python utils
 sudo apt install -y python3-pip 
 python3 -m pip install --upgrade pip
+python3 -m pip install --upgrade pipx
 python3 -m pip install --force --no-cache-dir --user -U pipenv 
 python3 -m pip install --force --no-cache-dir --user -U virtualenv
 python3 -m pip install --force --no-cache-dir --user -U virtualenvwrapper
