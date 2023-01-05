7b28a127935c764d13fddf42d976a77f00363982
MarcoFavorito <marco.favorito@gmail.com>
2023-01-05 15:47:14 +0100

    add gradle installation and env scripts

---
+++ gradle/env.sh
@@ -0,0 +1,2 @@
+export GRADLE_HOME=/opt/gradle/gradle-7.6
+export PATH=${GRADLE_HOME}/bin:${PATH}
---
+++ gradle/install.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+wget -c https://services.gradle.org/distributions/gradle-7.6-bin.zip -P /tmp
+sudo unzip -d /opt/gradle /tmp/gradle-7.6-bin.zip
